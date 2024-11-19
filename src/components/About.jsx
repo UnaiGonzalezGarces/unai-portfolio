05ff1b6986409a55e9e71a5172ad77c1997344a3
Unai González Garcés <unaiggarces@gmail.com>
2024-11-19 22:35:01 +0100

    Updated about me text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,30 @@
-const aboutItems = [
-  {
-    label: 'Project done',
-    number: 45,
-  },
-  {
-    label: 'Years of experience',
-    number: 10,
-  },
-];
+// const aboutItems = [
+//   {
+//     label: 'Project done',
+//     number: 45,
+//   },
+//   {
+//     label: 'Years of experience',
+//     number: 10,
+//   },
+// ];
 
 const About = () => {
   return (
     <section id='about' className='section'>
       <div className='container'>
         <div className='bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up'>
-          <p className='text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]'>
-            Welcome! I&apos;m Henry, a professional web developer with a knack
-            for crafting visually stunning and highly functional websites.
-            Combining creativity and technical expertise. I transform your
-            vision into digital masterpiece that excels in both appearance and
-            performance.
+          <p className='mb-4 text-zinc-300 md:mb-8 md:text-2xl'>
+            Hey there! I&apos;m{' '}
+            <span className='font-bold text-sky-400'>Unai González</span>, a
+            junior web developer based in Barcelona, Spain. In 2020 I started
+            studying computer science and there I discovered what I&apos;m
+            really passionate about, web development, since then I&apos;ve been
+            learning and working on my abilities. I&apos;m a very curious person
+            and I love to learn new things. I&apos;m always looking for new
+            challenges and opportunities to grow as a developer.
           </p>
-
+          {/*
           <div className='flex flex-wrap items-center gap-4 md:gap-7'>
             {aboutItems.map(({ label, number }, key) => (
               <div key={key}>
@@ -45,7 +48,7 @@ const About = () => {
               height={10}
               className='ml-auto md:w-[40px] md:h-[40px]'
             />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>

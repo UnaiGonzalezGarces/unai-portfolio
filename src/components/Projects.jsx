4502f000836cb7efc25563f60dfde2f2a596f0dc
Unai González Garcés <unaiggarces@gmail.com>
2024-11-19 23:02:23 +0100

    Projects title updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
   return (
     <section className='section' id='projects'>
       <div className='container'>
-        <h2 className='mb-8 headline-2 reveal-up'>My portfolio highlights</h2>
+        <h2 className='mb-8 headline-2 reveal-up'>Projects</h2>
 
         <div className='grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]'>
           {projects.map(({ imgSrc, title, tags, projectLink }, key) => (

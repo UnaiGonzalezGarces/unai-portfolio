06e236efad7d1c5581cb3154da079031ab5aa21a
Unai González Garcés <unaiggarces@gmail.com>
2024-11-19 22:50:48 +0100

    Updated skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,44 +2,24 @@ import SkillCard from './SkillCard';
 
 const skillsItem = [
   {
-    imgSrc: '/images/figma.svg',
-    label: 'Figma',
-    desc: 'Design tool',
-  },
-  {
     imgSrc: '/images/css3.svg',
     label: 'CSS',
-    desc: 'User Interface',
   },
   {
     imgSrc: '/images/javascript.svg',
     label: 'JavaScript',
-    desc: 'Interaction',
-  },
-  {
-    imgSrc: '/images/nodejs.svg',
-    label: 'NodeJS',
-    desc: 'Web Server',
-  },
-  {
-    imgSrc: '/images/expressjs.svg',
-    label: 'ExpressJS',
-    desc: 'Node Framework',
   },
   {
     imgSrc: '/images/mongodb.svg',
     label: 'MongoDB',
-    desc: 'Database',
   },
   {
     imgSrc: '/images/react.svg',
     label: 'React',
-    desc: 'Framework',
   },
   {
     imgSrc: '/images/tailwindcss.svg',
     label: 'TailwindCSS',
-    desc: 'User Interface',
   },
 ];
 
@@ -47,11 +27,10 @@ const Skills = () => {
   return (
     <section className='section'>
       <div className='container'>
-        <h2 className='headline-2 reveal-up'>Essential Tools I use</h2>
+        <h2 className='headline-2 reveal-up'>Some of my developement skills</h2>
 
-        <p className='text-zinc-400 mt-3 mb-8 max-w-[50ch] reveal-up'>
-          Discover the powerful tools and technologies I use to create
-          exceptional, high-performing websites & applications.
+        <p className='text-zinc-400 mt-3 mb-8 max-w-[50ch] md:max-w-full reveal-up font-bold'>
+          I have experience working with the following technologies and tools.
         </p>
 
         <div className='grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]'>
